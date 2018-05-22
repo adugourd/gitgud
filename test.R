1a672e677cf417f1b9247c6671b8cd7e66c9b643
adugourd <adugourd@ukaachen.de>
2018-05-22 11:35:38 +0200

    modified test and added test 2

--- test.R
+++ test.R
@@ -3,6 +3,8 @@ library(ggplot2)
 a <- 4
 b <- rnorm(a, 1, 100)
 
-c <- sample(c, 10, replace = FALSE)
+c <- sample(b, 10, replace = FALSE)
 
 plot(c)
+
+d <- sample(b, 10, replace = FALSE)
---
+++ test2.R
@@ -0,0 +1,8 @@
+library(ggplot2)
+
+a <- 7
+b <- rnorm(a, 2, 100)
+
+c <- sample(c, 20, replace = FALSE)
+
+plot(c)
